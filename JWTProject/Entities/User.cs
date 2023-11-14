f6d1d382bed29680bc8aeaaf3a97393a49eda5db
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-14 20:24:40 +0500

    Add project files.

---
+++ JWTProject/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTProject.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName {  get; set; }
+        public string PasswordHash {  get; set; }
+    }
+}
