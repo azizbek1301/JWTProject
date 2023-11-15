55111f707c4b12b7dfe60d01ea61aedbb37345b9
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-15 17:57:02 +0500

    Security password

---
+++ JWTProject/Services/Security/Hash512.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace JWTProject.Services.Security
+{
+    public class Hash512
+    {
+        public static string ComputeHash512(string password)
+        {
+            using(SHA512 sha512 = SHA512.Create())
+            {
+                byte[] inputBytes=Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes=sha512.ComputeHash(inputBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+        }
+    }
+}
