e0b39a2c1f35fa9deb3202b8983f03ca0bf58c37
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-15 17:57:13 +0500

    UserService

---
+++ JWTProject/Services/Users/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using JWTProject.Entities;
+using JWTProject.Models;
+
+namespace JWTProject.Services.Users
+{
+    public interface IUserService
+    {
+        public ValueTask<IEnumerable<User>> GetUsersAsync();
+        public ValueTask<bool> CreateAsync(UserCreateDto userModel);
+    }
+}
---
+++ JWTProject/Services/Users/UserService.cs
@@ -0,0 +1,34 @@
+﻿using JWTProject.DataAccess;
+using JWTProject.Entities;
+using JWTProject.Models;
+using JWTProject.Services.Security;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTProject.Services.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly DemoDbContext _context;
+        public UserService(DemoDbContext context)
+        {
+            _context = context;
+            
+        }
+        public async ValueTask<bool> CreateAsync(UserCreateDto userModel)
+        {
+            User user = new User();
+            user.UserName = userModel.UserName;
+            user.PasswordHash=Hash512.ComputeHash512(userModel.Password);
+
+            await _context.Users.AddAsync(user);
+            int result=await _context.SaveChangesAsync();
+            return result > 0;
+        }
+
+        public async ValueTask<IEnumerable<User>> GetUsersAsync()
+        {
+            var result = await _context.Users.ToListAsync();
+            return result;
+        }
+    }
+}
