9d3fbf3cc990d1af4c83e9e385d920965c8e84bc
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-15 18:32:18 +0500

    JwtProjetc Creat Get

--- JWTProject/Controllers/UserController.cs
+++ JWTProject/Controllers/UserController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JWTProject.Models;
+using JWTProject.Services.Users;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWTProject.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async ValueTask<IActionResult>GetAllAsync()
+        {
+            try
+            {
+                var users = await _userService.GetUsersAsync();
+                return Ok(users);   
+            }
+            catch (Exception ex)
+            {
+                return NotFound("User topilmadi");
+            }
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult>PostAsync(UserCreateDto createDto)
+        {
+            var res=await _userService.CreateAsync(createDto);
+            if (res)
+                return Ok("Success");
+            return BadRequest("Don't create");
+        }
     }
 }
--- JWTProject/Program.cs
+++ JWTProject/Program.cs
@@ -1,5 +1,10 @@
 using JWTProject.DataAccess;
+using JWTProject.Services.Author;
+using JWTProject.Services.Users;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +20,26 @@ builder.Services.AddDbContext<DemoDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
-//builder.Services.AddScoped<>;
+builder.Services.AddScoped<IAuthorService,AuthorService>();
+builder.Services.AddScoped<IUserService,UserService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            // kimga chiqarilgan
+            ValidateIssuer = true,
+            //kim tomonidan berilgan
+            ValidateAudience = true,
+            // vaqti
+            ValidateLifetime = true,
+            // Secret keyi
+            ValidateIssuerSigningKey = true,
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
