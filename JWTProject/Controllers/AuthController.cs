d5f5ba8b5ec94d0025086d0d761e05d0cc9555c1
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-15 17:55:31 +0500

    authcontroller

---
+++ JWTProject/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using JWTProject.Entities;
+using JWTProject.Models;
+using JWTProject.Services.Author;
+using JWTProject.Services.Security;
+using JWTProject.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IAuthorService _authService;
+        public AuthController(IUserService userService,IAuthorService authorService)
+        {
+            _userService = userService;
+            _authService = authorService;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> Login(UserCreateDto createDto)
+        {
+            var users=await _userService.GetUsersAsync();
+            User? user= users.FirstOrDefault(x=>x.UserName==createDto.UserName && x.PasswordHash==Hash512.ComputeHash512(createDto.Password));
+            if (user == null)
+            {
+                return NotFound("Login yoki password xato");
+            }
+            string token = _authService.GenerateToken(createDto);
+            return Ok(token);
+        }
+    }
+}
