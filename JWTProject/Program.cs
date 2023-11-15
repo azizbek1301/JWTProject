e53e0f43da1ac909573cf43bd5657fc15622aa4c
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-15 17:57:28 +0500

    Program.cs

--- JWTProject/Program.cs
+++ JWTProject/Program.cs
@@ -1,3 +1,6 @@
+using JWTProject.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DemoDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+//builder.Services.AddScoped<>;
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
